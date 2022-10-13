6421074833ac437129203edfb760753029a8af6a
Jeff <jcgilbert70@gmail.com>
2022-10-13 17:44:58 -0500

    created repo, added html, css, js & added some starter code & notes

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/* 
+
+1)  A) Game begins by cliking "Start" button
+
+2)  A) A timer starts
+    B) user is presented with a random question
+    C) When 1st question is answered, next question appears
+    D) A wrong answer deducts time from the clock
+
+3)  A) game ends when all questions done
+    B) game ends when time runs out
+
+4)  A) user enters initials for score
+    B) score and initals added to high score list
+    C) high score list can be refreshed
+
+*/
+
+
+// Array of questions
