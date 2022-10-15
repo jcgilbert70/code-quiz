3ef799d8cb34aba129af96b5725e08bd57bfe7a1
Jeff <jcgilbert70@gmail.com>
2022-10-15 16:44:19 -0500

    added frame of array of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,63 @@
     C) high score list can be refreshed
 
 */
+const quiz = [
+{
+    question: "question 1",
+    answers: ["answer1", "answer 2", "answer 3", "answer 4"]
+    correctAnswer: 3
+},
+{
+    question: "question 2",
+    answers: ["answer1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: 0
+},
+{
+    question: "question 3",
+    answers: ["answer1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: 3
+},
+{
+    question: "question 4",
+    answers: ["answer1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: 1
+},
+{
+    question: "question 5",
+    answers: ["answer1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: 2
+},
+]
+
+var score= 0;
+var time= 75;
+
+const timer= document.getElementById("timer");
+
+
 
 
 // Array of questions
+
+/*
+
+var questionList: []
+
+
+*/
+
+//-----------------------------------------------
+
+// function to start timer & display question onClick of start button
+
+// function when question is called, radom select from array
+
+// function if question answered wrong subtract 5 seconds from timer
+
+// function when all questions answered, stop timer & end game
+
+// function end game, show score & initial entry
+function endGame {
+    time= 0;
+
+}
