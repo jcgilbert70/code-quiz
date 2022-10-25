3bb474418d58831cec0ddddd57ac20a53bf451aa
Jeff <jcgilbert70@gmail.com>
2022-10-24 22:18:58 -0500

    restarting highscores logic again

--- assets/js/script.js
+++ assets/js/script.js
@@ -177,66 +177,15 @@ function endQuiz() {
     console.log("final score is: " + timeCount)
 }
 
+
 function printHighscores() { // 4-C) users initials and scores created in a list
+}
+console.log("printHighscores function started");
 
-    console.log("printHighscores function started");
-    var highScores = JSON.parse(localStorage.getItem("highscores"));
-    if (highScores != null) {
-        highScores.sort(function (a, b) {
-            return parseInt(b.score) - parseInt(a.score);
-        });
-        for (var i = 0; i < highScores.length; i++) {
-            var scoreLi = document.createElement("li");
-            scoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
-            document.getElementById("highscores").appendChild(scoreLi);
-        }
-
-    }
-    else {
-        var temp = document.getElementById("highscores");
-        temp.textContent = "NO HIGH SCORES";
-    }
-
-
-    function clearHighscores() {   // 4-D) high score list can be cleared
-        localStorage.removeItem("highscores");
-        location.reload();
-    }
-
-    var clearButton = document.getElementById("clear");
-    clearButton.addEventListener("click", function () {
-        clearHighscores();
-
-    })
-
+function clearHighscores() {   // 4-D) high score list can be cleared
 }
 
-
 function saveHighscore() { // 4-B) this function captures users initials and highscore
-    var initials = initialsEl.value.toUpperCase();
-    if (initials.length > 3) {
-        alert("Initials must be no more than 3 characters");
-        return;
-    }
-    else if (initials === " ") {
-        alert("Can not be blank");
-        return;
-    }
-    else {
-        var highscores;
-        if (JSON.parse(localStorage.getItem("highscores")) != null)
-            highscores = JSON.parse(window.localStorage.getItem("highscores"));
-        else
-            highscores = [];
-        var newScore = {
-            initials: initials,
-            score: timeCount
-        };
-        highscores.push(newScore);
-        localStorage.setItem("highscores", JSON.stringify(highscores));
-        location.href = "highscores.html";
-    }
-    printHighscores();
 }
 
 submitBtn.onclick = saveHighscore;
